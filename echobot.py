6ced3286322e31dbd6a846eaf3b70db077222d76
Roberto <roberto.arranz.93@gmail.com>
2019-05-29 19:39:43 +0200

    update

--- echobot.py
+++ echobot.py
@@ -1,5 +1,6 @@
 import json
 import requests
+import time
 
 TOKEN = "891596158:AAGpVgjNHUJujgiSf9D9Z5lAI2q0-6rTs98"
 URL = "https://api.telegram.org/bot{}/".format(TOKEN)
@@ -17,8 +18,17 @@ def get_json_from_url(url):
     return js
 
 
-def get_updates():
+def get_last_update_id(updates):
+    update_ids = []
+    for update in updates["result"]:
+        update_ids.append(int(update["update_id"]))
+    return max(update_ids)
+
+
+def get_updates(offset=None):
     url = URL + "getUpdates"
+    if offset:
+        url += "?offset={}".format(offset)
     js = get_json_from_url(url)
     return js
 
@@ -36,5 +46,24 @@ def send_message(text, chat_id):
     get_url(url)
 
 
-text, chat = get_last_chat_id_and_text(get_updates())
-send_message(text, chat)
+def echo_all(updates):
+    for update in updates["result"]:
+        try:
+            text = update["message"]["text"]
+            chat = update["message"]["chat"]["id"]
+            send_message(text, chat)
+        except Exception as e:
+            print(e)
+
+def main():
+    last_update_id = None
+    while True:
+        updates = get_updates(last_update_id)
+        if len(updates["result"]) > 0:
+            last_update_id = get_last_update_id(updates) + 1
+            echo_all(updates)
+        time.sleep(0.5)
+
+
+if __name__ == '__main__':
+    main()
